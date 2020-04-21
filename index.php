96af4b380a3d49d87a90c48ab6ed9715f4d784e9
jacobkorodimas <63014396+jacobkorodimas@users.noreply.github.com>
2020-04-21 15:28:59 -0600

    small styling

--- index.php
+++ index.php
@@ -12,17 +12,19 @@
     <?php include('templates/header.php');?>
 <body>
 
-    <?php foreach($exercises as $exercise): ?>
-
-        <div class='mb-5'>
-            <h4><?php echo ucfirst(strtolower($exercise['title']));?></h4>
-            <p><?php echo $exercise['setnum'];?> Sets</p>
-            <p><?php echo $exercise['repnum'];?> Reps</p>
-            <p>Category: <?php echo ucfirst(strtolower($exercise['category']));?></p>
-            <button><a href='edit.php?id=<?php echo $exercise['id']; ?>'>Edit</a></button>
-        </div>
-
-    <?php endforeach; ?>    
+    <div class='row'>
+        <?php foreach($exercises as $exercise): ?>
+        
+            <div class='col-3'>
+                <h4><?php echo ucfirst(strtolower($exercise['title']));?></h4>
+                <p><?php echo $exercise['setnum'];?> Sets</p>
+                <p><?php echo $exercise['repnum'];?> Reps</p>
+                <p class = 'text-muted'>Category: <?php echo ucfirst(strtolower($exercise['category']));?></p>
+                <button class='btn btn-info'><a id='editlink' href='edit.php?id=<?php echo $exercise['id']; ?>'>Edit</a></button>
+            </div>
+    
+        <?php endforeach; ?>    
+    </div>
 
 </body>
     <?php include('templates/footer.php');?>
